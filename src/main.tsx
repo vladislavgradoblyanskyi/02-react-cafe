0cde005020069b3ffaa1bd2f45f510655d17fe87
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-03-22 22:39:01 +0100

    try 10233

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '../src/components/App/App'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
