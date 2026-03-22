4a74445894f1d299f22a1f0112e0170486aa8076
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-03-22 22:36:37 +0100

    try 10233

---
+++ src/components/App/App.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+import CreateCafeInfoMarkup from '../CafeInfo/CafeInfo'
+import VoteOptions from '../VoteOptions/VoteOptions'
+import VoteStats from '../VoteStats/VoteStats'
+import Notification from '../Notification/Notification'
+
+import type { Votes } from '../../types/votes'
+import type { VoteType } from '../../types/votes'
+
+
+import css from './App.module.css'
+
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  function handleVote(type: VoteType) {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    })
+  }
+
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    })
+  }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0
+
+  return (
+    <div className={css.app}>
+      <CreateCafeInfoMarkup />
+
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,6 @@
+import css from './Notification.module.css'
+
+export default function Notification() {
+  return <p className={css.message}>No feedback yet</p>
+}
+
